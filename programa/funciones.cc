12c424d9e5b58c6627fde262bd39c28194e8c371
Elenaire <i82malue@uco.es>
2019-11-17 19:02:50 +0100

    He arregledo funciones de fecha y hora

--- programa/funciones.cc
+++ programa/funciones.cc
@@ -3,7 +3,7 @@
 void hoy(struct fecha &f){
 	time_t aux=time(0);
 	tm *t=localtime(&aux);
-	f.a=t->tm_year;
+	f.a=1900+t->tm_year;
 	f.m=t->tm_mon;
 	f.d=t->tm_mday;
 }
@@ -14,6 +14,29 @@ void hoy(struct hora &h){
 	h.m=t->tm_min;
 	h.s=t->tm_sec;
 }
+bool leerFecha(struct fecha &f){
+	if(scanf("%2d/%2d/%4d",f.d,f.m,f.a)==EOF){
+		return false;
+	}
+	return true;
+}
+bool leerHora(struct hora &h){
+	h.s=0;
+	if(scanf("%2d:%2d",h.h,h.m)==EOF){
+		return false;
+	}
+	return true;
+}
+
+char* escribeFecha(struct fecha const &f){
+	char c[11];
+	sprintf(c,"%2d/%2d/%4d",f.d,f.m,f.a);
+	return c;
+}
+char* escribeHora(struct hora const &h){
+	char c[5];
+	sprintf(c,"%2d:%2d",h.h,h.m);
+	return c;
 
 int dias(struct fecha f1,struct fecha f2){		//Devulve los dias transcurridos desde f1 hasta f2, si ha transcurrido mas de un mes devulve 100, f2 es anterios a f1 devuelve un numero negativo
 		int mes[12]={31,28,31,30,31,30,31,31,30,31,30,31};
--- programa/funciones.h
+++ programa/funciones.h
@@ -15,6 +15,9 @@
 
 	void hoy(struct fecha &f);			//Actualiza f con la fecha actual
 	void hoy(struct hora &h);			//Actualiza h con la hora actual
-	bool leerFecha(struct fecha &f);		//--Lee d/m/a por teclado y devulve false si la fecha introducida no es correcta -Sin crear -Sin Usar
-	bool leerHora(struct hora &h);			//--Lee h:m:s por teclado y devulve false si la hora introducida no es correcta -Sin crear -Sin Usar
+	bool leerFecha(struct fecha &f);		//--Lee d/m/a por teclado y devulve false si la fecha introducida no es correcta -Sin probar -Sin Usar
+	bool leerHora(struct hora &h);			//--Lee h:m por teclado y devulve false si la hora introducida no es correcta -Sin probar -Sin Usar
+	char* escribeFecha(struct fecha const &f);		//-Sin probar
+	char* escribeHora(struct hora const &h);		//-Sin probar
 	int dias(struct fecha f1,struct fecha f2);		//Devulve los dias transcurridos desde f1 hasta f2, si ha transcurrido mas de un mes devulve 100, f2 es anterios a f1 devuelve -1
+	int minutos(struct hora h1,struct hora h2);		//-Sin crear
