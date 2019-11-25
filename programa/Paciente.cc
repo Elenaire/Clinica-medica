5230bca9328ffb519263f7faadbb37cd072b1b41
ReSeT <55441370+ReSeTPFL@users.noreply.github.com>
2019-11-25 17:15:15 +0100

    Añadidos los Paciente.c y .h de Rafael Varona

--- programa/Paciente.cc
+++ programa/Paciente.cc
@@ -1,20 +1,95 @@
-#include "Paciente.h"
+#include "Pacientes.h"
 
-	private:
-		int id_;				//Numero para poder seleccionar al paciente cuando se está buscando
-		string nombre_;
-		string apellidos_;
-		string direccion_;
-		struct fecha fechanacimiento_;
-		int telefono_;
-		int codpostal_;
-		int tipo_;
-
-list <Tratamiento> Paciente::getTratamientos(){
-	/*Lee del fichero de tratamientos del paciente una lista de tratamientos*/
+Paciente::Paciente(int telefono)
+{
+  id_=0;
+  telefono_=telefono;
+  nombre_="";
+  apellidos_="";
+  direccion_="";
+  fechanacimiento_->a=0;
+  fechanacimiento_->m=0;
+  fechanacimiento_->d=0;
+  codpostal_=0;
+  tipo_=-1;
 }
 
+void Paciente::mostrarPaciente()
+{
+  cout<<"ID: "<<id_<<endl;
+  cout<<"NOMBRE: "<<nombre_<<endl;
+  cout<<"APELLIDOS: "<<apellidos_<<endl;
+  cout<<"DIRECCION: "<<direccion_<<endl;
+  char* fecha=escribeFecha(fechanacimiento_);
+  cout<<"FECHANACIMIENTO: "<<fecha<<endl;
+  cout<<"TELEFONO: "<<telefono_<<endl;
+  cout<<"CODIGO POSTAL: "<<codpostal_<<endl;
+  if(tipo_=0)
+  {
+    cout<<"TIPO: PUBLICO"<<endl;
+  }else{
+    cout<<"TIPO: PRIVADO"<<endl;
+  }
+}
 
-bool Paciente::addTratamiento(Tratamiento t){
-	//Inserta el tratamiento al final del fichero de tratamientos del paciente, si por algun motivo no puede devulve false
+list <Cita> Paciente::getCitas()
+{
+  list <Cita> aux;
+  list <Cita>::iterator c;
+  ifstream file("citas.txt")
+
+
+
+
+}
+
+void Paciente::AddPaciente(Paciente p)
+{
+  list<Paciente> aux;
+  list<Paciente>::iterator i;
+  ifstream file;
+  file.open("Pacientes.txt");
+  string cad;
+  for (i=aux.begin();i!=aux.end();i++)
+  {
+    getline(file,cad,"||");
+    i->id_=stoi(cad);
+    getline(file,cad,"||");
+    i->nombre_=cad;
+    getline(file,cad,"||");
+    i->apellidos_=cad;
+    getline(file,cad,"||");
+    i->direccion_=cad;
+    getline(file,cad,"/");
+    (i->fechanacimiento_)->d=stoi(cad);
+    getline(file,cad,"/");
+    (i->fechanacimiento_)->m=stoi(cad);
+    getline(file,cad,"||");
+    (i->fechanacimiento_)->a=stoi(cad);
+    getline(file,cad,"||");
+    i->telefono_=stoi(cad);
+    getline(file,cad,"||");
+    i->codpostal_=stoi(cad);
+    getline(file,cad,"\n");
+    i->tipo_=stoi(cad);
+    aux.push_back(i);
+  }
+  file.close();
+  i++;
+  aux.resize(i,p);
+  ofstream archivo;
+  archivo.open("Pacientes.txt");
+  for (i=aux.begin();i!=aux.end();i++)
+  {
+    archivo<<i->id_<<"||";
+    archivo<<i->nombre_<<"||";
+    archivo<<i->apellidos_<<"||";
+    archivo<<(i->fechanacimiento_)->d<<"/";
+    archivo<<(i->fechanacimiento_)->m<<"/";
+    archivo<<(i->fechanacimiento_)->a<<"||";
+    archivo<<i->telefono_<<"||";
+    archivo<<i->codpostal_<<"||";
+    archivo<<i->tipo_<<endl;
+  }
+  archivo.close();
 }
--- programa/Paciente.h
+++ programa/Paciente.h
@@ -1,41 +1,55 @@
 #ifndef PACIENTE_H
-	#define PACIENTE_H
-	#include <string>
-	#include <list>
-	#include "funciones.h"
-	#include "Cita.h"
-	#include "Tratamiento.h"
-	#include "Nota.h"
-	class Paciente{
-	private:
-		int id_;				//Numero para poder seleccionar al paciente cuando se está buscando
-		string nombre_;
-		string apellidos_;
-		string direccion_;
-		struct fecha fechanacimiento_;
-		int telefono_;
-		int codpostal_;
-		int tipo_;				//Publico 0, Privado 1, otro posible tipo otro numero
-	public:
-		Paciente();				//Constructor  (Tengo que mirar si se pueden hacer 2 constructores, uno pasandole los datos y solo con la id y que lo lea de un fichero)
-		int getID()const {
-			return id_;
-		}
-		// get y set
-		getCitas();
-		list <Tratamiento> getTratamientos();
-		getNotas();
-		mostrarPaciente();		//Muestra por pantalla los datos del paciente
-		mostrarTratamientos ();		// Muestra los tratamientos actuales
-		mostrarHCitas ();		// Muestra el historial de citas
-		mostrarHTratamientos ();	// Muestra el historial de tratamientos
-		mostrarHNotas ();		// Muestra el historial de notas
-		mostrarHistorial ();		// Muestra el historial de citas, estudios y notas ordenadas por fecha sin distinguir el tipo
-		addCita();
-		bool addTratamiento(Tratamiento t);
-		addNota();
-		guardarPaciente();		//Añade o modifica al paciente en el fichero de pacientes
-		borrarPaciente();		//Elimina al paciente del fichero de pacientes y elimina todos sus datos
-		-Paciente();			//Destructor
-	}
+#define PACIENTE_H
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <list>
+#include "funciones.h"
+#include "Cita.h"
+#include "Tratamiento.h"
+#include "Nota.h"
+
+class Paciente
+{
+private:
+  list <Cita> citas_;
+  list <Cita>::iterator c_;
+  list <Tratamiento> tratamientos_;
+  list <Tratamiento>::iterator t_;
+  list <Notas> notas_;
+  list <Notas>::iterator n_;
+  int id_;
+  string nombre_;
+  string apellidos_;
+  string direccion_;
+  struct fecha fechanacimiento_;
+	int telefono_;
+	int codpostal_;
+	int tipo_;
+public:
+  Paciente(int telefono);
+  inline int getID(){return id_;}
+  inline string getNombre(){return nombre_;}
+  inline string getApellidos(){return apellidos_;}
+  inline string getDireccion(){return direccion_;}
+  inline struct fecha getFechanacimiento(){return fechanacimiento_;}
+  inline int getTelefono(){return telefono_;}
+  inline int getCodPostal(){return codpostal_;}
+  inline int getTipo(){return tipo_;}
+  inline void setID(int id){id_=id;}
+  inline void setNombre(string name){nombre_=name;}
+  inline void setApellidos(string surname){apellidos_=surname;}
+  inline void setDireccion(string adress){nombre_=name;}
+  inline void setFechanacimiento(struct fecha date){fechanacimiento_=date;}
+  inline void setTelefono(int tlf){telefono_=tlf;}
+  inline void setCodPostal(int cp){codpostal_=cp;}
+  inline void setTipo(int type){tipo_=type;}
+  list <Cita> getCitas();
+  list <Tratamiento> getTratamiento();
+  list <Nota> getNotas();
+  void mostrarPaciente();
+
+};
+
 #endif
